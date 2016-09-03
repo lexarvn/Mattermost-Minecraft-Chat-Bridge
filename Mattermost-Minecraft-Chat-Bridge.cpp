df8c4fbf33efe4b1d2f6ae124eb095bdc8cd62e8
lexarvn <lexarvn@gmail.com>
2016-09-02 17:42:27 -0700

    Duplicate changes?

--- Mattermost-Minecraft-Chat-Bridge.cpp
+++ Mattermost-Minecraft-Chat-Bridge.cpp
@@ -1,148 +1,234 @@
 #include <iostream>
+#include <algorithm>
+#include <future>
 #include <stdlib.h>
-#include "Simple-Web-Server/server_http.hpp"
-
-//Added for the json-example
 #define BOOST_SPIRIT_THREADSAFE
+#include <boost/regex.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-
-//Added for the default_resource example
-#include <fstream>
-#include <boost/filesystem.hpp>
-#include <array>
-#include <algorithm>
+#include <curl/curl.h>
+#include "Simple-Web-Server/server_http.hpp"
+#include "inotify-cxx/inotify-cxx.cpp"
 
 using namespace std;
-//Added for the json-example:
-using namespace boost::property_tree;
-
 typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+
+unsigned char from_hex(unsigned char ch)
+{
+  if (ch <= '9' && ch >= '0')
+  ch -= '0';
+  else if (ch <= 'f' && ch >= 'a')
+  ch -= 'a' - 10;
+  else if (ch <= 'F' && ch >= 'A')
+  ch -= 'A' - 10;
+  else
+  ch = 0;
+  return ch;
+}
+
+string urldecode(const string& str)
+{
+  string result;
+  string::size_type i;
+  for (i = 0; i < str.size(); ++i)
+  {
+    if (str[i] == '+')
+    {
+      result += ' ';
+    }
+    else if (str[i] == '%' && str.size() > i+2)
+    {
+      const unsigned char ch1 = from_hex(str[i+1]);
+      const unsigned char ch2 = from_hex(str[i+2]);
+      const unsigned char ch = (ch1 << 4) | ch2;
+      result += ch;
+      i += 2;
+    }
+    else
+    {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+map<string, string> parse_url(std::string word)
+{
+  string::size_type pos;
+  map<string, string> queries;
+
+  for (pos = 0; pos < word.size(); ++pos)
+  {
+    if (word[pos] == '&')
+    word[pos] = ' ';
+  }
+
+  istringstream sin(word);
+  sin >> word;
+  while (sin)
+  {
+    pos = word.find_first_of("=");
+    if (pos != std::string::npos)
+    {
+      string key = urldecode(word.substr(0,pos));
+      string value = urldecode(word.substr(pos+1));
+
+      queries[key] = value;
+    }
+    sin >> word;
+  }
+  return queries;
+}
+
+string sendMsgToMinecraft(string request, string outToken, string service, string mm2m)
+{
+  auto params  = parse_url(request);
+
+  auto team_id      = params["team_id"];
+  auto team_domain  = params["team_domain"];
+
+  auto channel_id   = params["channel_id"];
+  auto channel_name = params["channel_name"];
+
+  auto user_id      = params["user_id"];
+  auto user_name    = params["user_name"];
+
+  auto trigger_word = params["trigger_word"];
+  auto text         = params["text"];
+
+  auto timestamp    = params["timestamp"];
+  auto token        = params["token"];
+
+  boost::regex re1("\"");
+  boost::regex re2("'");
+  text = boost::regex_replace(text, re1, "\\\\u0022");
+  text = boost::regex_replace(text, re2, "\\\\u0027");
+
+  auto command = "service "+service+" command 'tellraw @a {\"text\":\"<"+mm2m+":"+user_name+"> "+text+"\"}'";
+  system(command.c_str());
+
+  cout << command << endl;
+
+  return "HTTP/1.1 200 OK\r\nContent-Length: 0 \r\n\r\n";
+}
+
+void sendMsgToMattermost(string logMsg, string webhook, string m2mm, string iconUrl)
+{
+  boost::smatch what;
+  boost::regex expression("^\\[[0-9]{2}:[0-9]{2}:[0-9]{2}\\] \\[Server thread\\/INFO\\]: <([^>]+)> (.+)$");
+  if(boost::regex_match(logMsg, what, expression))
+  {
+    string username = what[1];
+    string message = what[2];
+
+    boost::regex re("\"");
+    message = boost::regex_replace(message, re, "\\\\u0022");
+
+    string payload = "payload={\"username\":\""+m2mm+":"+username+"\",\"icon_url\": \""+iconUrl+"\",\"text\":\""+message+"\"}";
+
+    CURL *curl;
+    CURLcode res;
+
+    res = curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, webhook.c_str());
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    cout << payload << endl;
+  }
+}
 
 int main()
 {
-	//HTTP-server at port 8080 using 4 threads
-    HttpServer server(8087, 4);
+  boost::property_tree::ptree config;
+  int port, threads;
+  string outToken, webhook, mc_log, service, m2mm, mm2m, iconUrl;
+  try
+  {
+    boost::property_tree::read_json("config.json", config);
 
-    //Add resources using path-regex and method-string, and an anonymous function
-    //POST-example for the path /string, responds the posted string
-    server.resource["^/string$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
-        //Retrieve string:
-        auto content=request->content.string();
-        //request->content.string() is a convenience function for:
-        //stringstream ss;
-        //ss << request->content.rdbuf();
-        //string content=ss.str();
+    port    = config.get<int>("port");
+    threads = config.get<int>("threads");
 
-        response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-    };
+    outToken= config.get<string>("token");
+    service = config.get<string>("minecraft_service");
+    mm2m    = config.get<string>("mattermost_to_minecraft_prefix");
 
-    //POST-example for the path /json, responds firstName+" "+lastName from the posted json
-    //Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
-    //Example posted json:
-    //{
-    //  "firstName": "John",
-    //  "lastName": "Smith",
-    //  "age": 25
-    //}
-    server.resource["^/json$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
-        try {
-            ptree pt;
-            read_json(request->content, pt);
+    webhook = config.get<string>("webhook");
+    mc_log  = config.get<string>("minecraft_log");
+    m2mm    = config.get<string>("minecraft_to_mattermost_prefix");
+    iconUrl = config.get<string>("icon_url");
+  }
+  catch(exception& e)
+  {
+    cout << "Error parsing config.json: " << e.what() << endl;
+    return 1;
+  }
 
-            string name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
+  HttpServer server(port, threads);
+  server.default_resource["POST"] = server.default_resource["GET"] = [outToken,service,mm2m](HttpServer::Response& response, shared_ptr<HttpServer::Request> request)
+  {
+    try
+    {
+      response << sendMsgToMinecraft(request->content.string(),outToken,service,mm2m);
+    }
+    catch(exception& e)
+    {
+      response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
+    }
+  };
 
-            response << "HTTP/1.1 200 OK\r\nContent-Length: " << name.length() << "\r\n\r\n" << name;
-        }
-        catch(exception& e) {
-            response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
-        }
-    };
+  auto future = async(launch::async, [&server](){ server.start(); });
 
-    //GET-example for the path /info
-    //Responds with request-information
-    server.resource["^/info$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
-        stringstream content_stream;
-        content_stream << "<h1>Request from " << request->remote_endpoint_address << " (" << request->remote_endpoint_port << ")</h1>";
-        content_stream << request->method << " " << request->path << " HTTP/" << request->http_version << "<br>";
-        for(auto& header: request->header) {
-            content_stream << header.first << ": " << header.second << "<br>";
-        }
+  condition_variable cv;
+  atomic<bool> readLog(false);
+  atomic<bool> running(true);
+  Inotify notify;
+  InotifyWatch watch(mc_log, IN_MODIFY);
+  notify.Add(watch);
 
-        //find length of content_stream (length received using content_stream.tellp())
-        content_stream.seekp(0, ios::end);
+  thread worker([&cv,&running,&readLog,mc_log,webhook,m2mm,iconUrl]()
+  {
+    mutex mtx;
+    std::unique_lock<std::mutex> lck(mtx);
+    ifstream initLog(mc_log);
+    string line;
+    while (initLog >> ws && getline(initLog, line)); //get the last line
+    initLog.seekg(0, initLog.end);
+    auto oldPosition = initLog.tellg();
+    initLog.close();
 
-        response <<  "HTTP/1.1 200 OK\r\nContent-Length: " << content_stream.tellp() << "\r\n\r\n" << content_stream.rdbuf();
-    };
+    while(running)
+    {
+        cv.wait(lck,[&readLog](){return readLog.load();});
+        readLog.store(false);
 
-    //GET-example for the path /match/[number], responds with the matched string in path (number)
-    //For instance a request GET /match/123 will receive: 123
-    server.resource["^/match/([0-9]+)$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
-        string number=request->path_match[1];
-        response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
-    };
+        ifstream logFile(mc_log);
+        logFile.seekg(0, logFile.end);
+        auto newPosition = logFile.tellg();
+        logFile.seekg(0, logFile.beg);
+        if(oldPosition <= newPosition) logFile.seekg(oldPosition);
 
-    //Default GET-example. If no other matches, this anonymous function will be called.
-    //Will respond with content in the web/-directory, and its subdirectories.
-    //Default file: index.html
-    //Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
-    server.default_resource["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
-        const auto web_root_path=boost::filesystem::canonical("web");
-        boost::filesystem::path path=web_root_path;
-        path/=request->path;
-        if(boost::filesystem::exists(path)) {
-            path=boost::filesystem::canonical(path);
-            //Check if path is within web_root_path
-            if(distance(web_root_path.begin(), web_root_path.end())<=distance(path.begin(), path.end()) &&
-               equal(web_root_path.begin(), web_root_path.end(), path.begin())) {
-                if(boost::filesystem::is_directory(path))
-                    path/="index.html";
-                if(boost::filesystem::exists(path) && boost::filesystem::is_regular_file(path)) {
-                    ifstream ifs;
-                    ifs.open(path.string(), ifstream::in | ios::binary);
+        do {
+          getline(logFile, line);
+          logFile >> ws;
+          sendMsgToMattermost(line, webhook, m2mm, iconUrl);
+        } while(logFile.good());
 
-                    if(ifs) {
-                        ifs.seekg(0, ios::end);
-                        size_t length=ifs.tellg();
+        oldPosition = newPosition;
+        logFile.close();
+    }
+  });
 
-                        ifs.seekg(0, ios::beg);
+  for (auto status = future.wait_for(chrono::milliseconds(0)); status != future_status::ready; status = future.wait_for(chrono::milliseconds(0))) {
+    notify.WaitForEvents();
+    readLog.store(true);
+    cv.notify_one();
+  }
 
-                        response << "HTTP/1.1 200 OK\r\nContent-Length: " << length << "\r\n\r\n";
-
-                        //read and send 128 KB at a time
-                        const size_t buffer_size=131072;
-                        array<char, buffer_size> buffer;
-                        size_t read_length;
-                        try {
-                            while((read_length=ifs.read(&buffer[0], buffer_size).gcount())>0) {
-                                response.write(&buffer[0], read_length);
-                                response.flush();
-                            }
-                        }
-                        catch(const exception &e) {
-                            cerr << "Connection interrupted, closing file" << endl;
-                        }
-
-                        ifs.close();
-                        return;
-                    }
-                }
-            }
-        }
-        string content="Could not open path "+request->path;
-        response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-    };
-
-    thread server_thread([&server](){
-        //Start server
-        server.start();
-    });
-
-    //Wait for server to start so that the client can connect
-    this_thread::sleep_for(chrono::seconds(1));
-
-    server_thread.join();
-
-    system("service minecraft command 'tellraw @p {\"text\":\"<mattermost:username> Hello World!\"}'");
-	return 0;
+  return 0;
 }
